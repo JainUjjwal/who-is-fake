28c1cebf313157b28de8a50fe6deda1bcdc3ef52
ujjwal jain <ujjwal.jain1012@gmail.com>
2025-03-30 20:16:11 -0700

    Update SERVER_URL to live backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // IMPORTANT: Make sure this URL matches your backend server URL!
 // If your backend runs on port 3001, this is correct.
 // const SERVER_URL = 'http://localhost:3001';
-const SERVER_URL = 'PLACEHOLDER_BACKEND_URL';
+const SERVER_URL = 'https://who-is-fake.onrender.com';
 // Create the socket instance
 // 'autoConnect: false' prevents it from connecting immediately on load.
 // We will call socket.connect() manually when the user joins or creates a room.
