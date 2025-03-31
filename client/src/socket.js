7d7095a2ed04e0e39fae47a86c0ae063166c7cef
ujjwal jain <ujjwal.jain1012@gmail.com>
2025-03-30 20:07:43 -0700

    Configure client for GitHub Pages deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 
 // IMPORTANT: Make sure this URL matches your backend server URL!
 // If your backend runs on port 3001, this is correct.
-const SERVER_URL = 'http://localhost:3001';
-
+// const SERVER_URL = 'http://localhost:3001';
+const SERVER_URL = 'PLACEHOLDER_BACKEND_URL';
 // Create the socket instance
 // 'autoConnect: false' prevents it from connecting immediately on load.
 // We will call socket.connect() manually when the user joins or creates a room.
