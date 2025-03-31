4e19a8e8658618f40eabb01ca0ee4a1d620482a2
ujjwal jain <ujjwal.jain1012@gmail.com>
2025-03-30 20:47:43 -0700

    forgot dumb import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LobbyPage from './pages/LobbyPage';
 import GamePage from './pages/GamePage';
